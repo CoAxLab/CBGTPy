fec2fb8ee0a5fd166adab7cf5fbf0bd7a06d4cf6
Jyotika Bahuguna <jyotika.bahuguna@gmail.com>
2023-12-12 18:39:38 +0100

    create so and mv them to correct directories

--- setup.py
+++ setup.py
@@ -1,14 +1,40 @@
-from setuptools import setup
+from setuptools import setup, Extension
 from Cython.Build import cythonize
+import os
+import glob
+
+cwd = os.getcwd()
+cwd = '/'.join(cwd.split('/')[:-1])
+
+extensions_nchoice = [Extension(name="agent_timestep_plasticity", 
+    sources=
+    [cwd+"/nchoice/"+'agent_timestep_plasticity.pyx',
+    ],
+)]
+
+extensions_stop = [Extension(name="agent_timestep_stop_signal", 
+    sources=
+    [cwd+"/stopsignal/"+'agent_timestep_stop_signal.pyx',
+    ],
+)]
+
 
 setup(
     name='nchoice',
-    ext_modules=cythonize("../nchoice/agent_timestep_plasticity.pyx"),
+    ext_modules=cythonize(extensions_nchoice,build_dir="./"),
     zip_safe=False,
 )
 
 setup(
     name='stopsignal',
-    ext_modules=cythonize("../stopsignal/agent_timestep_stop_signal.pyx"),
+    ext_modules=cythonize(extensions_stop,build_dir="./"),
     zip_safe=False,
 )
+
+so_files = glob.glob(cwd+"/notebooks/"+"*.so")
+for sf in so_files:
+    print(sf)
+    if "plasticity" in sf:
+        os.replace(sf, cwd+"/nchoice/agent_timestep_plasticity.so")
+    elif "stop_signal" in sf:
+        os.replace(sf, cwd+"/stopsignal/agent_timestep_stop_signal.so")
